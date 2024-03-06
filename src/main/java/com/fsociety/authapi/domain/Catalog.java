e1d62752eabdde7ed23ed3b80456720ef20fe401
sxpjb <emmanuel.hernandezramirez@gmail.com>
2024-03-06 16:02:21 -0600

    [SXPJB] add new unit test

--- src/main/java/com/fsociety/authapi/domain/Catalog.java
+++ src/main/java/com/fsociety/authapi/domain/Catalog.java
@@ -9,13 +9,17 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.util.UUID;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "catalog")
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Catalog {
 
   @Id
---
+++ src/test/java/com/fsociety/authapi/unit/app/register/RegisterServiceImplTest.java
@@ -0,0 +1,77 @@
+package com.fsociety.authapi.unit.app.register;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.fsociety.authapi.app.catalog.CatalogService;
+import com.fsociety.authapi.app.emailprovider.EmailService;
+import com.fsociety.authapi.app.register.impl.RegisterServiceImpl;
+import com.fsociety.authapi.domain.Catalog;
+import com.fsociety.authapi.domain.PersonRepository;
+import com.fsociety.authapi.domain.User;
+import com.fsociety.authapi.domain.UserRepository;
+import com.fsociety.authapi.domain.dto.UserRequestBuilder;
+import com.fsociety.authapi.domain.dto.UserRequestDTO;
+import com.fsociety.authapi.domain.dto.UserResponseDTO;
+import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+public class RegisterServiceImplTest {
+  @Mock private EmailService emailService;
+  @Mock private CatalogService catalogService;
+  @Mock private UserRepository userRepository;
+  @Mock private PersonRepository personRepository;
+  @InjectMocks private RegisterServiceImpl registerService;
+
+  @BeforeEach
+  void setUp() throws Exception {
+    MockitoAnnotations.openMocks(this);
+    when(catalogService.findByGenderByCode(anyString()))
+        .thenReturn(new Catalog(UUID.randomUUID(), "c_catalog", "M", "Male", true));
+  }
+
+  private UserRequestDTO buildUserFromRequest() {
+    UserRequestDTO userRequestDTO = new UserRequestDTO();
+    userRequestDTO.setUsername("username");
+    userRequestDTO.setPassword("password");
+    userRequestDTO.setFirstName("firstName");
+    userRequestDTO.setLastName("lastName");
+    userRequestDTO.setEmail("email");
+    userRequestDTO.setGender("M");
+    return userRequestDTO;
+  }
+
+  @Test
+  void registerUserSuccessfully() throws Exception {
+    UserRequestDTO userRequestDTO = buildUserFromRequest();
+    User user =
+        new UserRequestBuilder()
+            .fromUserRegisterDTO(userRequestDTO)
+            .withGender(userRequestDTO.getGender(), catalogService)
+            .withExpirationCode()
+            .build();
+    when(personRepository.save(any())).thenReturn(user.getPerson());
+    when(userRepository.save(any())).thenReturn(user);
+
+    UserResponseDTO result = registerService.register(userRequestDTO);
+
+    verify(userRepository).save(any());
+    verify(personRepository).save(any());
+    verify(emailService).sendConfirmationEmail(anyString(), anyString(), anyString());
+
+    assertNotNull(result);
+    assertEquals(user.getUsername(), result.getUsername());
+    assertEquals(user.getPerson().getFirstName(), result.getFirstName());
+    assertEquals(user.getPerson().getLastName(), result.getLastName());
+    assertEquals(user.getPerson().getEmail(), result.getEmail());
+    assertEquals(user.getPerson().getGender().getCode(), result.getGender());
+  }
+}
